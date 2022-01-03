c2c378f6794eda2acdd7a854210d2d9e54937f1a
Xhianto <xhianto@gmail.com>
2022-01-03 23:11:42 +0100

    MainMenu

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -16,12 +18,19 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.SignInButton
+import com.google.android.gms.tasks.OnCompleteListener
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    var mGoogleSignInClient: GoogleSignInClient? = null
+    lateinit var mGoogleSignInClient: GoogleSignInClient
     lateinit var buttonSignIn: SignInButton
     lateinit var textLoginNotice: TextView
+    lateinit var settingsImageButton: ImageButton
+    lateinit var buttonSignOut: Button
+    lateinit var buttonEasy: Button
+    lateinit var buttonHard: Button
+    lateinit var buttonP1vsP2: Button
+    lateinit var user: User
 
     private var launchLoginActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
@@ -37,8 +46,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         buttonSignIn = findViewById(R.id.sign_in_button)
         buttonSignIn.setOnClickListener(this)
-        textLoginNotice = findViewById(R.id.textBlock)
+        buttonSignOut = findViewById(R.id.signout)
+        buttonSignOut.setOnClickListener(this)
+        buttonEasy = findViewById(R.id.easy)
+        buttonEasy.setOnClickListener(this)
+        buttonHard = findViewById(R.id.hard)
+        buttonHard.setOnClickListener(this)
+        buttonP1vsP2 = findViewById(R.id.p1vsp2)
+        buttonP1vsP2.setOnClickListener(this)
+        settingsImageButton = findViewById(R.id.settingsButton)
+        settingsImageButton.setOnClickListener(this)
+        textLoginNotice = findViewById(R.id.TextLoginNotice)
         textLoginNotice.text = getString(R.string.no_login)
+
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -50,22 +70,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
-    private fun signIn() {
-        val signInIntent = mGoogleSignInClient!!.signInIntent
-        launchLoginActivity.launch(signInIntent)
-        //startActivityForResult(signInIntent, RC_SIGN_IN)
+    override fun onStart() {
+        super.onStart()
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignIn.getLastSignedInAccount(this)
+        signIn()
     }
 
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.sign_in_button -> signIn()
-        }
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        launchLoginActivity.launch(signInIntent)
+        //startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
-
+            user = User(-1, account.displayName, account.email)
+            buttonSignIn.visibility = View.INVISIBLE
+            textLoginNotice.visibility = View.INVISIBLE
+            buttonEasy.visibility = View.VISIBLE
+            buttonHard.visibility = View.VISIBLE
+            buttonP1vsP2.visibility = View.VISIBLE
+            buttonSignOut.visibility = View.VISIBLE
+            settingsImageButton.visibility = View.VISIBLE
             // Signed in successfully, show authenticated UI.
             //updateUI(account)
             textLoginNotice.text = account.displayName
@@ -75,6 +105,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             //Log.w(TAG, "signInResult:failed code=" + e.statusCode)
             //updateUI(null)
             textLoginNotice.text = e.statusCode.toString()
+        }
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this) {
+                buttonSignIn.visibility = View.VISIBLE
+                textLoginNotice.visibility = View.VISIBLE
+                buttonEasy.visibility = View.INVISIBLE
+                buttonHard.visibility = View.INVISIBLE
+                buttonP1vsP2.visibility = View.INVISIBLE
+                buttonSignOut.visibility = View.INVISIBLE
+                settingsImageButton.visibility = View.INVISIBLE
+                Toast.makeText(this@MainActivity, "Signed Out Successfully", Toast.LENGTH_LONG)
+                    .show()
+            }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.sign_in_button -> signIn()
+            R.id.signout -> signOut()
         }
     }
 }
---
+++ app/src/main/java/com/example/tictactoe/User.kt
@@ -0,0 +1,35 @@
+package com.example.tictactoe
+
+import java.io.Serializable
+
+class User : Serializable {
+    var userId = 0
+    var username: String? = null
+    var emailAddress: String? = null
+    var easyWin = 0
+    var easyDraw = 0
+    var easyLose = 0
+    var hardWin = 0
+    var hardDraw = 0
+    var hardLose = 0
+    var friendWin = 0
+    var friendDraw = 0
+    var friendLose = 0
+
+    constructor(userId: Int, username: String?, emailAddress: String?) {
+        this.userId = userId
+        this.username = username
+        this.emailAddress = emailAddress
+        easyWin = 0
+        easyDraw = 0
+        easyLose = 0
+        hardWin = 0
+        hardDraw = 0
+        hardLose = 0
+        friendWin = 0
+        friendDraw = 0
+        hardLose = 0
+    }
+
+    constructor() {}
+}
